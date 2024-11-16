300acf3fb0a9ec04867c763e83855938d0589997
Pomazoff <stpomazov@mail.ru>
2024-11-16 10:38:19 +0300

    Попытка сделать базу данных в другом файле.

--- kassa/js/database.js
+++ kassa/js/database.js
@@ -1,28 +1,9 @@
 const data = [
-    {
-        id: 123,
-        title: 'Молоко',
-        price: 100,
-        src: '',
-    },
-    {
-        id: 321,
-        title: 'Шоколад',
-        price: 50,
-        src: '',
-    },
-    {
-        id: 231,
-        title: 'Вода',
-        price: 24,
-        src: '',
-    },
-    {
-        id: 412,
-        title: 'Хлеб',
-        price: 33,
-        src: '',
-    },
-]
-
-export default data
+    { id: 123, title: 'Молоко', price: 100, src: 'https://cdn.onlinewebfonts.com/svg/img_85921.png' },
+    { id: 321, title: 'Шоколад', price: 50, src: 'https://avatars.mds.yandex.net/i?id=3f0a43e96711cf9f8e51a21d219e43181163cdbb-12544737-images-thumbs&n=13' },
+    { id: 231, title: 'Вода', price: 24, src: 'https://avatars.mds.yandex.net/i?id=bdb6d0f7006e0668b4989d5b812b2cb4-5516535-images-thumbs&n=13' },
+    { id: 412, title: 'Хлеб', price: 33, src: 'https://avatars.mds.yandex.net/i?id=802d00c8b425bffb02403d1d11797f71e4980a074cca1516-12218216-images-thumbs&n=13' },
+    { id: 132, title: 'Квас', price: 90, src: 'https://thumbs.dreamstime.com/b/beer-icon-file-eps-format-36691198.jpg' },
+    { id: 122, title: 'Сыр', price: 250, src: 'https://i.pinimg.com/736x/62/e1/c1/62e1c1590c0792a42670e62070181b62.jpg' },
+];
+export {data};
